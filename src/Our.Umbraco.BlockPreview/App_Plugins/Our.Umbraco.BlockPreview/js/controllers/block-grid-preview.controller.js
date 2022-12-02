ba76066a69ff0d73d9c6e91f68fdf5b4703ca478
Rick Butterfield <Rick.Butterfield@sigma.se>
2022-12-02 14:32:28 +0000

    Initial commit

---
+++ src/Our.Umbraco.BlockPreview/App_Plugins/Our.Umbraco.BlockPreview/js/controllers/block-grid-preview.controller.js
@@ -0,0 +1,38 @@
+﻿angular.module('umbraco').controller('Our.Umbraco.BlockPreview.Controllers.BlockGridPreviewController',
+    ['$scope', '$sce', '$timeout', 'editorState', 'Our.Umbraco.BlockPreview.Resources.PreviewResource',
+        function ($scope, $sce, $timeout, editorState, previewResource) {
+            var active = editorState.getCurrent().variants.find(function (v) {
+                return v.active;
+            });
+
+            if (active !== null) {
+                if (active.language !== null) {
+                    $scope.language = active.language.culture;
+                }
+            }
+
+            $scope.id = editorState.getCurrent().id;
+            $scope.loading = true;
+            $scope.markup = $sce.trustAsHtml('Loading preview');
+
+            function loadPreview(blockData) {
+                $scope.markup = $sce.trustAsHtml('Loading preview');
+                $scope.loading = true;
+
+                previewResource.getGridPreview(blockData, $scope.id, $scope.language).then(function (data) {
+                    $scope.markup = $sce.trustAsHtml(data);
+                    $scope.loading = false;
+                });
+            }
+
+            var timeoutPromise;
+
+            $scope.$watch('block.data', function (newValue, oldValue) {
+                $timeout.cancel(timeoutPromise);
+
+                timeoutPromise = $timeout(function () {   //Set timeout
+                    loadPreview(newValue);
+                }, 500);
+            }, true);
+        }
+    ]);
---
+++ src/Our.Umbraco.BlockPreview/App_Plugins/Our.Umbraco.BlockPreview/js/resources/preview.resource.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    function previewResource($http, umbRequestHelper) {
+
+        var gridApiUrl = "/umbraco/backoffice/api/BlockGridPreviewApi/PreviewMarkup";
+        var listApiUrl = "/umbraco/backoffice/api/BlockListPreviewApi/PreviewMarkup";
+
+        var resource = {
+            getGridPreview: getGridPreview,
+            getListPreview: getListPreview
+        };
+
+        return resource;
+
+        function getGridPreview(data, pageId, culture) {
+
+            return umbRequestHelper.resourcePromise(
+                $http.post(gridApiUrl + '?pageId=' + pageId + '&culture=' + culture, data),
+                'Failed getting block grid preview markup'
+            );
+        };
+
+        function getListPreview(data, pageId, culture) {
+
+            return umbRequestHelper.resourcePromise(
+                $http.post(listApiUrl + '?pageId=' + pageId + '&culture=' + culture, data),
+                'Failed getting block list preview markup'
+            );
+        };
+    }
+
+    angular.module('umbraco.resources').factory('Our.Umbraco.BlockPreview.Resources.PreviewResource', previewResource);
+
+})();
