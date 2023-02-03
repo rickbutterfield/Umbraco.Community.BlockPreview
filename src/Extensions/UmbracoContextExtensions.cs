db4cc112c4018d74e910467a043d339cc5d5a547
Aaron <aaron@umbhost.net>
2023-02-03 14:26:39 +0000

    Changed to _ as it's not used

--- src/Extensions/UmbracoContextExtensions.cs
+++ src/Extensions/UmbracoContextExtensions.cs
@@ -6,7 +6,7 @@ namespace Our.Umbraco.BlockPreview.Extensions
 {
     public static class UmbracoContextExtensions
     {
-        public static bool IsBlockPreview(this IUmbracoContext umbracoContext, HttpContext httpContext)
+        public static bool IsBlockPreview(this IUmbracoContext _, HttpContext httpContext)
         {
             var requestControllerName = (string)httpContext.Request.RouteValues["controller"] + "Controller";
             if (requestControllerName.Equals	(nameof(BlockPreviewApiController)) && httpContext.Request.RouteValues["action"].Equals	(nameof(BlockPreviewApiController.PreviewMarkup)))
