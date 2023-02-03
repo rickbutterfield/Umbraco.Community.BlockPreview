35fc7b712cdf485fd97eb6f9c1566329f12befae
Aaron <aaron@umbhost.net>
2023-02-03 12:24:22 +0000

    Added IsBlockPreview

---
+++ src/Extensions/UmbracoContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Umbraco.Cms.Core.Web;
+
+namespace Our.Umbraco.BlockPreview.Extensions
+{
+    public static class UmbracoContextExtensions
+    {
+        public static bool IsBlockPreview(this IUmbracoContext umbracoContext)
+        {
+            if (umbracoContext.OriginalRequestUrl.LocalPath.Contains("blockpreviewapi",
+                    StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
