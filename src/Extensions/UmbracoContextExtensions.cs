ce81d2b1ea80a85ddd2b6660f76af6bed16ba7e7
Aaron <aaron@umbhost.net>
2023-02-03 14:24:37 +0000

    Changed to use nameof instead of checking the URL
    It should be safer in case of future changes

--- src/Extensions/UmbracoContextExtensions.cs
+++ src/Extensions/UmbracoContextExtensions.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using Our.Umbraco.BlockPreview.Controllers;
 using Umbraco.Cms.Core.Web;
+using HttpContext = Microsoft.AspNetCore.Http.HttpContext;
 
 namespace Our.Umbraco.BlockPreview.Extensions
 {
     public static class UmbracoContextExtensions
     {
-        public static bool IsBlockPreview(this IUmbracoContext umbracoContext)
+        public static bool IsBlockPreview(this IUmbracoContext umbracoContext, HttpContext httpContext)
         {
-            if (umbracoContext.OriginalRequestUrl.LocalPath.Contains("blockpreviewapi",
-                    StringComparison.InvariantCultureIgnoreCase))
+            var requestControllerName = (string)httpContext.Request.RouteValues["controller"] + "Controller";
+            if (requestControllerName.Equals	(nameof(BlockPreviewApiController)) && httpContext.Request.RouteValues["action"].Equals	(nameof(BlockPreviewApiController.PreviewMarkup)))
             {
                 return true;
             }
-
             return false;
         }
     }
