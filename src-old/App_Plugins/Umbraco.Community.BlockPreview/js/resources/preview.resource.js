a760b5f59fb41fb4841de78dd612a5c69c4e0347
Rick Butterfield <Rick.Butterfield@nexergroup.com>
2023-05-24 12:25:49 +0100

    🎉 v1.2.2

---
+++ src-old/App_Plugins/Umbraco.Community.BlockPreview/js/resources/preview.resource.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    function previewResource($http, umbRequestHelper) {
+
+        var apiUrl = Umbraco.Sys.ServerVariables.UmbracoCommunityBlockPreview.PreviewApi;
+
+        var resource = {
+            getPreview: getPreview,
+        };
+
+        return resource;
+
+        function getPreview(data, pageId, isGrid, culture) {
+            culture = culture || '';
+
+            return umbRequestHelper.resourcePromise(
+                $http.post(apiUrl + '?pageId=' + pageId + '&isGrid=' + isGrid + '&culture=' + culture, data),
+                'Failed getting block preview markup'
+            );
+        };
+    }
+
+    angular.module('umbraco.resources').factory('Umbraco.Community.BlockPreview.Resources.PreviewResource', previewResource);
+
+})();
