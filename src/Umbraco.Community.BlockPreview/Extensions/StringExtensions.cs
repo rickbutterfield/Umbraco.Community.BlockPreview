139eb18bbbec156d8c84ab6ba89bef02e9d0918c
Arkadiusz Biel - bielu <bielu@bielu.pl>
2024-01-16 09:47:44 +0000

    fix: use more corrected name for extension

---
+++ src/Umbraco.Community.BlockPreview/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Umbraco.Community.BlockPreview.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToPascalCase(this string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return $"{char.ToUpper(value[0], CultureInfo.CurrentCulture)}{value[1..]}";
+    }
+}
