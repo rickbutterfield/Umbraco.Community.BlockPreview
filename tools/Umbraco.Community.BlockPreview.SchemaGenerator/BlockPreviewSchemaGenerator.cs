d68e106bed8180f617eb3caf85bd2e65418e30da
Rick Butterfield <rcb@umbraco.dk>
2025-11-21 10:05:17 +0000

    Correctly generate schema on build

--- tools/Umbraco.Community.BlockPreview.SchemaGenerator/BlockPreviewSchemaGenerator.cs
+++ tools/Umbraco.Community.BlockPreview.SchemaGenerator/BlockPreviewSchemaGenerator.cs
@@ -1,78 +1,46 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
-using NJsonSchema.Generation;
-using NJsonSchema.NewtonsoftJson.Generation;
+﻿using NJsonSchema.Generation;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Umbraco.Community.BlockPreview.SchemaGenerator
 {
-    internal class BlockPreviewSchemaGenerator
+  internal class BlockPreviewSchemaGenerator
+  {
+    private readonly JsonSchemaGenerator _schemaGenerator;
+
+    public BlockPreviewSchemaGenerator()
+        => _schemaGenerator = new JsonSchemaGenerator(new BlockPreviewSchemaGeneratorSettings());
+
+    public string Generate()
     {
-        private readonly JsonSchemaGenerator _schemaGenerator;
-
-        public BlockPreviewSchemaGenerator()
-        {
-            _schemaGenerator = new JsonSchemaGenerator(new BlockPreviewSchemaGeneratorSettings());
-        }
-
-        public string Generate()
-        {
-            var blockPreviewSchema = GenerateBlockPreviewSchema();
-            return blockPreviewSchema.ToString();
-        }
-
-        private JObject GenerateBlockPreviewSchema()
-        {
-            var schema = _schemaGenerator.Generate(typeof(AppSettings));
-            return JsonConvert.DeserializeObject<JObject>(schema.ToJson());
-        }
+      var blockPreviewSchema = GenerateBlockPreviewSchema();
+      return blockPreviewSchema.ToString();
     }
 
-    internal class BlockPreviewSchemaGeneratorSettings : NewtonsoftJsonSchemaGeneratorSettings
+    private JsonObject GenerateBlockPreviewSchema()
     {
-        public BlockPreviewSchemaGeneratorSettings()
-        {
-            AlwaysAllowAdditionalObjectProperties = true;
-            SerializerSettings = new JsonSerializerSettings()
-            {
-                ContractResolver = new WritablePropertiesOnlyResolver(),
-            };
-            DefaultReferenceTypeNullHandling = ReferenceTypeNullHandling.NotNull;
-            SchemaNameGenerator = new NamespacePrefixedSchemaNameGenerator();
-            SerializerSettings.Converters.Add(new StringEnumConverter());
-            IgnoreObsoleteProperties = true;
-            GenerateExamples = true;
-        }
-
-        private class WritablePropertiesOnlyResolver : DefaultContractResolver
-        {
-            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-            {
-                IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
-                var result = props.Where(p => p.Writable).ToList();
-                result.ForEach(x => x.PropertyName = ToPascalCase(x.PropertyName));
-                return result;
-            }
-
-            /// <summary>
-            ///  we serialize everything camel case inside uSync but the settings are actually PascalCase 
-            ///  for appsettings.json, so we need to PascalCase each property. 
-            /// </summary>
-            private string ToPascalCase(string str)
-            {
-                if (!string.IsNullOrEmpty(str))
-                {
-                    return char.ToUpperInvariant(str[0]) + str.Substring(1);
-                }
-
-                return str;
-            }
-        }
+      var schema = _schemaGenerator.Generate(typeof(AppSettings));
+      return JsonSerializer.Deserialize<JsonObject>(schema.ToJson());
     }
+  }
 
-    internal class NamespacePrefixedSchemaNameGenerator : DefaultSchemaNameGenerator
+  internal class BlockPreviewSchemaGeneratorSettings : SystemTextJsonSchemaGeneratorSettings
+  {
+    public BlockPreviewSchemaGeneratorSettings()
     {
-        public override string Generate(Type type) => type.Namespace.Replace(".", string.Empty) + base.Generate(type);
+      AlwaysAllowAdditionalObjectProperties = true;
+      SerializerOptions = new JsonSerializerOptions();
+      DefaultReferenceTypeNullHandling = ReferenceTypeNullHandling.NotNull;
+      SchemaNameGenerator = new NamespacePrefixedSchemaNameGenerator();
+      SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+      IgnoreObsoleteProperties = true;
+      GenerateExamples = true;
     }
+  }
+
+  internal class NamespacePrefixedSchemaNameGenerator : DefaultSchemaNameGenerator
+  {
+    public override string Generate(Type type) => type.Namespace.Replace(".", string.Empty) + base.Generate(type);
+  }
 }
