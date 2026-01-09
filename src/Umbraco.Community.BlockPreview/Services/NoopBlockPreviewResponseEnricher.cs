97963b8efcb5991c837ae052641a256e2050cd03
Søren Kottal <sk@ecreo.dk>
2026-01-09 11:47:47 +0100

    Adds IBlockPreviewResponseEnricher

---
+++ src/Umbraco.Community.BlockPreview/Services/NoopBlockPreviewResponseEnricher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Community.BlockPreview.Interfaces;
+
+namespace Umbraco.Community.BlockPreview.Services
+{
+    /// <inheritdoc />
+    public class NoopBlockPreviewResponseEnricher : IBlockPreviewResponseEnricher
+    {
+        /// <inheritdoc />
+        public Task<string> EnrichAsync(
+            string markup,
+            HttpContext httpContext,
+            IPublishedContent? content,
+            string? blockEditorAlias = null,
+            string? contentElementAlias = null,
+            string? contentUdi = null,
+            string? settingsUdi = null,
+            int? blockIndex = null
+        ) => Task.FromResult(markup);
+    }
+}
